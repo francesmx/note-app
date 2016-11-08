9175e1b854aef9090ca146377e9a4a0a5e358958
James <James.Steel@me.com>
2016-11-08 12:31:58 +0000

    added tests for no notes and many notes

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -6,3 +6,21 @@ function testNoteListView() {
 }
 
 testNoteListView();
+
+function testNoteListViewWithNoNotes() {
+  var noteList = new NoteList();
+  var noteListView = new NoteListView(noteList);
+  assert.isTrue(noteListView.getHTML() === "<ul></ul>");
+}
+testNoteListViewWithNoNotes()
+
+
+function testNoteListViewWithManyNotes() {
+  var noteList = new NoteList();
+  noteList.createNote("hi there");
+  noteList.createNote("again?");
+  noteList.createNote("hello");
+  var noteListView = new NoteListView(noteList);
+  assert.isTrue(noteListView.getHTML() === "<ul><li><div>hi there</div></li><li><div>again?</div></li><li><div>hello</div></li></ul>");
+}
+testNoteListViewWithManyNotes()
