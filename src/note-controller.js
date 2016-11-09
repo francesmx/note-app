915a0ec791eaffffd118592bdc964c6a0095a4e3
Frances Maxwell <fmaxwell@gmail.com>
2016-11-09 16:36:15 +0000

    Almost there with single page view thingie

--- src/note-controller.js
+++ src/note-controller.js
@@ -4,6 +4,27 @@
     this.noteListView = new NoteListView();
   }
 
+  makeURLChangeShowNoteForCurrentPage();
+
+  function makeURLChangeShowNoteForCurrentPage() {
+    window.addEventListener("hashchange", showNoteForCurrentPage);
+  }
+
+  function showNoteForCurrentPage() {
+    showNote(getNoteIDFromUrl(window.location));
+  }
+
+  function getNoteIDFromUrl(location) {
+    return location.hash.split("#notes/")[1];
+  }
+
+  function showNote(note_id) {
+    var note = this.noteList.findNoteById(note_id);
+    console.log(note);
+    var singleNoteView = new SingleNoteView(note);
+    document.getElementById("app").innerHTML = singleNoteView.renderNote();
+  }
+
   NoteController.prototype.insertHTML = function() {
     var html = this.noteListView.renderAll(this.noteList)
     document.getElementById("app").innerHTML=html
--- src/note-list-model.js
+++ src/note-list-model.js
@@ -1,18 +1,25 @@
 (function(exports) {
   function NoteList() {
     this.notes = []
-    this.noteID = 1
   }
 
   NoteList.prototype.addNote = function(text) {
-    var note = new Note(text, this.noteID);
-    this.noteID++;
+    var note = new Note(text);
     this.notes.push(note);
   };
 
   NoteList.prototype.returnNotes = function() {
     return this.notes;
   };
+
+  NoteList.prototype.findNoteById = function(id) {
+
+    function hasId(value) {
+      return value.id === id;
+    }
+
+    return note = this.notes.find(hasId);
+  }
 
   exports.NoteList = NoteList;
   exports.addNote = this.addNote;
