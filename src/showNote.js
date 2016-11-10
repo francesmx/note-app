fdacff4eed2134b80a143a51c86e0691e767d19a
glynester <glynester@hotmail.com>
2016-11-10 13:23:19 +0000

    Url changes when you click on a link.

---
+++ src/showNote.js
@@ -0,0 +1,17 @@
+
+
+  function makeUrlChangeShowNoteForCurrentPage() {
+    window.addEventListner("hashchange", showNoteForCurrentPage);
+  };
+
+  function showNoteForCurrentPage(){
+    showNote(getNoteFromUrl(window.location));
+  };
+
+  function getNoteFromUrl(winLocation) {
+    return winLocation.hash.split("#")[1];
+  }
+
+  function showNote(noteRef){
+    document.getElementById('app').innerHTML = "This is a test note as you can tell."
+  }
